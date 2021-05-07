abe16c50dab2e97180c6ce72600dc9847d143362
Klaas Decock <klaasdc@icloud.com>
2021-05-07 15:49:56 +0200

    header component deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ItemComponent } from './item/item.component';
 import {HttpClientModule} from "@angular/common/http";
-import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ItemComponent,
-    HeaderComponent
   ],
   imports: [
     BrowserModule,
